014f7246a2cebbc11c132500f1cd1179151ef53b
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-09-02 19:50:13 +0530

    Add files via upload

---
+++ Linearsearch.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int key = 4;
+    for (int i = 0; i < 9; i++)
+    {
+        if (A[i] == key)
+        {
+            cout << "key found in " << A[i] << " position" << endl;
+        }
+    }
+
+    return 0;
+}
